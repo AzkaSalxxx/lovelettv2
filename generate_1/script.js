c26eecfe406aa6c5ae833125a6a4f0bdb890367b
Azka Salxxx <ghifars96@gmail.com>
2026-06-05 23:03:21 +0700

    Update script.js

--- generate_1/script.js
+++ generate_1/script.js
@@ -4,7 +4,7 @@ let idx = 0;
 let timer;
 
 const $ = (id) => document.getElementById(id);
-const cfg = window.CONFIG || CONFIG;
+const cfg = window.CONFIG || CONFIG1;
 
 function setText(id, text) {
   const el = $(id);
