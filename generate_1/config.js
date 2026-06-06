337c91a1f441fa1e2da3feff55163d906b4bfe4a
Azka Salxxx <ghifars96@gmail.com>
2026-06-06 14:01:32 +0700

    Update config.js

--- generate_1/config.js
+++ generate_1/config.js
@@ -15,23 +15,24 @@ const CONFIG1 = {
   typeSpeed: 32,
   message: `Aku bikin halaman kecil ini untuk menyimpan rasa yang kadang susah diucapkan langsung.
 
-Semoga saat kamu buka surat ini, kamu senyum sebentar dan merasa dihargai. Terima kasih sudah hadir, dengan semua hal sederhana yang ternyata berarti besar.
+Semoga saat kamu buka surat ini, kamu senyum sebentar dan merasa dihargai. Terima kasih sudah hadir, dengan semua hal sederhana yang ternyata berarti besar. Terimakasih untuk semua waktu, kebahagiaan, uang yang kamu keluarkan dan untuk semua yang kamu usahakan untukku.
 
-Jangan lupa, kamu selalu punya tempat spesial di cerita ini.`,
+Jangan lupa, kamu selalu punya tempat spesial di hatiku .
+Loveyou ayang`,
 
   galleryTitle: "Photo Collage",
   galleryDesc: "Klik foto, lalu geser pakai tombol kanan/kiri tanpa harus back.",
   footerText: "Made with love.",
 
   photos: [
-    { src: "generate_1/photos/foto1.jpg", caption: "Memory 01" },
-    { src: "generate_1/photos/foto2.jpg", caption: "Sweet 02" },
-    { src: "generate_1/photos/foto3.jpg", caption: "Love 03" },
-    { src: "generate_1/photos/foto4.jpg", caption: "Smile 04" },
-    { src: "generate_1/photos/foto5.jpg", caption: "Dream 05" },
-    { src: "generate_1/photos/foto6.jpg", caption: "Forever 06" },
-    { src: "generate_1/photos/foto7.jpg", caption: "Us 07" },
-    { src: "generate_1/photos/foto8.jpg", caption: "Happy 08" }
+    { src: "generate_1/photos/foto1.jpg", caption: "Memory " },
+    { src: "generate_1/photos/foto2.jpg", caption: "Sweet " },
+    { src: "generate_1/photos/foto3.jpg", caption: "Love " },
+    { src: "generate_1/photos/foto4.jpg", caption: "Smile " },
+    { src: "generate_1/photos/foto5.jpg", caption: "Dream " },
+    { src: "generate_1/photos/foto6.jpg", caption: "Forever " },
+    { src: "generate_1/photos/foto7.jpg", caption: "Us " },
+    { src: "generate_1/photos/foto8.jpg", caption: "Happy " }
   ],
 
   hearts: {
